e46323669073fb480eb1a00ac787798f44dc20f2
Michael Kohler <m_t_k@gmx.de>
2016-06-09 21:56:47 +0200

    [Project] Fix VS2015 x64 build script

--- build_solution_VS2015_x64.cmd
+++ build_solution_VS2015_x64.cmd
@@ -14,7 +14,7 @@ if not defined DevEnvDir (
 if not exist %build_dir% mkdir %build_dir% 
 cd %build_dir%
 
-cmake -G "Visual Studio 14 2014" -DCMAKE_INSTALL_PREFIX="./%build_dir%" ".."
+cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX="./%build_dir%" ".."
 msbuild /M asplib_playground.sln
 cmake --build . --target INSTALL
 
